19969f2eaf5d24efa27ec023d80beba939e7afdd
lintingjie <993091886@qq.com>
2018-08-28 23:11:36 +0800

    同步阻塞IO

---
+++ src/chapter2/nio/TimeServerHandler.java
@@ -0,0 +1,64 @@
+package chapter2.nio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * Created by ltj on 18-8-28
+ */
+public class TimeServerHandler implements Runnable {
+
+    private Socket  socket;
+
+    public TimeServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            out = new PrintWriter(this.socket.getOutputStream(), true);//autoFlush
+            String currentTime = null;
+            String body = null;
+            while(true){
+                body = in.readLine();
+                if(body == null){
+                    break;
+                }
+                System.out.println("The time server receive order:"+body);
+                currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
+                        new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+                out.println(currentTime);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(in != null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(out != null){
+                out.close();
+                out = null;
+            }
+            if(this.socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                this.socket = null;
+            }
+        }
+    }
+}
