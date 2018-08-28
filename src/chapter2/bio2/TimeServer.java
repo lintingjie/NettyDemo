166a7b490a8a7f79d3a24a79ce6430a02fca8c7a
lintingjie <993091886@qq.com>
2018-08-29 00:13:00 +0800

    伪异步IO示例

---
+++ src/chapter2/bio2/TimeServer.java
@@ -0,0 +1,8 @@
+package chapter2.aio;
+
+/**
+ * 伪异步IO的TimeServer
+ * Created by ltj on 18-8-28
+ */
+public class TimeServer {
+}
---
+++ src/chapter2/bio2/TimeServerHandlerExecutePool.java
@@ -0,0 +1,7 @@
+package chapter2.bio2;
+
+/**
+ * Created by ltj on 18-8-28
+ */
+public class TimeServerHandlerExecutePool {
+}
