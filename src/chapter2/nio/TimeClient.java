1d9adb9302f55f7af8e9a3da506e7756b945d7dc
lintingjie <993091886@qq.com>
2018-09-01 15:09:34 +0800

    NIO客户端

---
+++ src/chapter2/nio/TimeClient.java
@@ -0,0 +1,13 @@
+package chapter2.nio;
+
+/**
+ * Created by lintingjie on 2018/9/1.
+ */
+
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        new Thread(new TimeClientHandle("127.0.0.1", port), "TimeClient-001").start();
+    }
+}
